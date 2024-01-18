2f5f3237edc5a30d6d2d31fb61109d1067c73452
amitj1jan <amitj1jan@gmail.com>
2024-01-18 17:23:21 +0400

    updated requirement.txt, provided inline comments for code

--- src/apps/localLLM_cmtPrompt.py
+++ src/apps/localLLM_cmtPrompt.py
@@ -6,22 +6,37 @@ import sys
 # conda environment for this app - 
 # conda activate llm
 # command to run this app
-# python3 src/apps/localLLM_cmtPrompt.py -w
+# python3 src/apps/localLLM_cmtPrompt.py
 
+# how many tokens are processed in parallel.
 n_batch = 256
-n_gpu_layers = 40
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# determines how many layers of the model are offloaded to your GPU.
+# Change this value based on your model and your GPU VRAM pool.
+n_gpu_layers = 30
+
+# path to offline downloaded llm model 
 modelpath = "models/llama-2-7b-chat.Q2_K.gguf"
 
 
 llm = LlamaCPP(
         model_path = modelpath,
+        # this parameter controls the response's factualness to creativity(its range is: 0-1), 
+        # i.e. lower the value, more factual response would be and higher the value, more creative a response would be.
         temperature=0.1,
-        max_new_tokens=3000,
         
+        max_new_tokens=3000,         
+        # llama2 has a context window of 4096 tokens, but I set it lower to allow for some wiggle room    
         context_window=3900,
+    
+        # kwargs to pass to __call__()
         generate_kwargs = {},
-        model_kwargs = {"n_gpu_layers": n_gpu_layers},
+    
+        # kwargs to pass to __init__()
+        model_kwargs = {"n_gpu_layers": n_gpu_layers,
+                           "n_batch": n_batch},
+        
+        # transform inputs into Llama2 format
         messages_to_prompt = messages_to_prompt,
         completion_to_prompt=completion_to_prompt,
         verbose=True,
@@ -29,7 +44,6 @@ llm = LlamaCPP(
 
 
 # Start chatting with the chatbot
-chat_history = []
 while True:
     query = input('Prompt: ')
     start_time = datetime.now()
@@ -37,10 +51,11 @@ while True:
         print("Exiting")
         sys.exit()
 
+    # generates the response based on the query provided
     response = llm.complete(query)
+    
     end_time = datetime.now()
     time_taken = end_time - start_time
     print("total time taken was:", time_taken)
     print('Answer: ' + response.text + '\n')
-    chat_history.append((query, response.text))
 
